3f0c60dc64d86f9d98d9be2d2c3087cd8ce32516
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-08-24 23:12:58 +0300

    Get elements

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,14 @@
+// Get our elements
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
+const progressBar = player.querySelector('.progress_filled')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelector('[data-skip]')
+const ranges = player.querySelector('.player_slider')
+
+
+
+// Build out functions
+
+// Hook up the event listeners
